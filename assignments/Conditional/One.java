33bef86eae1458466b1d81688357dd4624e96585
keerthi-rkl <keerthiagustd.p@gmail.com>
2022-03-08 22:52:36 +0530

    commit

---
+++ assignments/Conditional/One.java
@@ -0,0 +1,10 @@
+class One {
+    public static void main(String[] args) {
+        int a = 14;
+        if (a % 7 == 0) {
+            System.out.println(a + " is divisible by 7");
+        } else {
+            System.out.println(a + " is not divisible by 7");
+        }
+    }
+}
--- iterative/One.java
+++ iterative/One.java
@@ -10,7 +10,7 @@ class One {
     public static void main(String[] args) {
         for (int i = 1; i <= 10; i++) {
             System.out.println(i);
-            for (int k : i) {
+            for (k of i) {
                 System.out.println(k);
             }
         }
